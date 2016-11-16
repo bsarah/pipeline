e91382d3957f4678d7345d59a63be717d61e9b1f
Sarah Berkemer <bsarah@bioinf.uni-leipzig.de>
2016-11-16 14:52:43 +0100

    draw graphs file

---
+++ scripts_sarah/drawGraphs.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl -w
+
+##perl drawGraphs.pl graphfile more_options outfile
+
+##this program is thought as an external program to only draw specified
+##graph files if the user wants to.
+
+##TODO write this code
