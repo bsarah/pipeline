12a67c27b49977875075047985ba8615e4bcc962
Cameron Murray <cameron@rum.bioinf.uni-leipzig.de>
2016-11-16 01:35:47 +0100

    Completed the pipeing alteration to preprocessing. Added one new file, maf2TempBed.py

--- scripts_cam/maf2TempBed.py
+++ scripts_cam/maf2TempBed.py
@@ -3,71 +3,96 @@ import subprocess
 import argparse
 from os import listdir
 from os.path import isfile, join
-from dataStructure_lessMem import catchExceptions
+from parser_MAF_lessMem import catchExceptions
 
 parser = argparse.ArgumentParser()
-parser.add_argument("blockNum", type=int, help="current block number between all maf files")
-parser.add_argument("mafDir", type=str)
-parser.add_argument("iteration", type=int)
-parser.add_argument("writeFilesDir", type=str)
+#parser.add_argument("iteration", type=int)
+#parser.add_argument("mafDir", type=str)
+#parser.add_argument("mafFile",type=str)
+parser.add_argument("blockNumber", type=int)
+parser.add_argument("speciesDir", type=str)
 
 args = parser.parse_args()
 
-blockNum = args.blockNum
-iteration = args.iteration
-listSpeciesFiles = [join(args.writeFilesDir, f) for f in listdir(args.writeFilesDir) if isfile(join(args.writeFilesDir, f))]
-listMafFiles = [join(args.mafDir, f) for f in listdir(args.mafDir) if isfile(join(args.mafDir, f))]
+#mafFiles = [join(args.mafDir,f) for f in listdir(args.mafDir) if isfile(join(args.mafDir, f))]
+#mafFile = args.mafFile #mafFiles[args.iteration]
+
+listSpeciesFiles = [join(args.speciesDir,f) for f in listdir(args.speciesDir) if isfile(join(args.speciesDir, f))]
+
+blockNum = args.blockNumber
+
+#print("converting maf files to temp files...")
+lastLineA = False
+
+writeFiles = list()
+for _file in listSpeciesFiles:
+    #print('file: {}'.format(_file))
+    appendFile = open(_file, 'a')
+    #print(type(appendFile))
+    writeFiles.append(appendFile)
 
 for line in sys.stdin:
-    
-    print("converting maf files to temp files...")
-    lastLineA = False
 
-    if line[0] not in {'#','a','s','i','e','q',' ','\n'}:
-        print("Ignoring {}. File not of maf format".format(readFile.name))
-        break
+        if line[0] not in {'#','a','s','i','e','q',' ','\n'}:
+            #print("Ignoring {}. File not of maf format".format(readFile.name))
+            break
 
-    if line[0] == 'a':
-        blockNum += 1
-        lastLineA = True
-        buf = line.split()[1]
-               #read score, with and without a decimal place
-                                                                       
-        if '.' in buf.split('=')[1]:
-            score = int(float(buf.split('=')[1]))
-        else:
-            score = int(buf.split('=')[1])
 
-    if line[0] == 's':
-        buf = line.split()
-        ID = buf[1].split('.')
+        if line[0] == 'a':
+            blockNum += 1
+            lastLineA = True
+            buf = line.split()[1]
+            #read score, with and without a decimal place
 
-        try:
-            test = ID[1]
-        except IndexError:
-            print("Ignoring {}. File lacks chromosome information".format(readFile.name))
-            break #break out of for line in readFile for loop, continue to next filename in mafdir (recursive call)                                                                         
-        else:
-            i = 0
-            for writeFile in listSpeciesFiles:
-                writeFile = open(writeFile, 'w')
-                i+=1
-                numSearches = len(listSpeciesFiles)
+            str_score = buf.split('=')[1]
+            if '.' in str_score:
+                score = int(float(str_score))
+            else:
+                score = int(str_score)
 
-                writeFileName = writeFile.name.split('/')[-1]
-                if writeFileName.startswith(ID[0]):#ex: does dm6_temp.bed start with dm6                                                                                                            
-                    writeFile.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(\
-                                    ID[1],ID[0]+'_'+str(blockNum), buf[2], buf[3], buf[4], lastLineA, score))
-                    #               chr   species_blocknum         start   length  strand  isRefSpecies allignmentScore
+        if line[0] == 's':
+            buf = line.split()
+            ID = buf[1].split('.')
 
-                elif i > numSearches:
-                    print("writeFileName: {}".format(writeFileName))
-                    raise Exception("the species of a maf allignment was not in the list of species given\n"+\
-                                    "allignment species: {}".format(ID[0]))
-                lastLineA = False
-    print("maf file read")
+            try:
+                test = ID[1]
+            except IndexError:
+                #print("Ignoring {}. File lacks chromosome information".format(readFile.name))
+                break #break out of for line in readFile for loop, continue to next filename in mafdir (recursive call)
+            else:
+                i = 0
+                for writeFile in writeFiles:
+                    #writeFile = open(writeFile, 'w')
+                    #writeFile.seek(0, 2)
+                    i+=1
+                    numSearches = len(listSpeciesFiles)
 
-#Call next iteration
-#1 Recursive call for each maf file
-catchExceptions(listMafFiles[args.iteration+1])
-subprocess.call("zcat "+listMafFiles[args.iteration+1]+" | python3 maf2TempBed.py "+str(blockNum)+" "+args.mafDir+" "+str(iteration+1))
+                    writeFileName = writeFile.name.split('/')[-1]
+                    if writeFileName.startswith(ID[0]):#ex: does dm6_temp.bed start with dm6
+                        writeFile.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(\
+                                        ID[1],ID[0]+'_'+str(blockNum), buf[2], buf[3], buf[4], lastLineA, score))
+                        #               chr   species_blocknum         start   length  strand  isRefSpecies allignmentScore
+                    elif i > numSearches:
+                        #print("writeFileName: {}".format(writeFileName))
+                        raise Exception("the species of a maf allignment was not in the list of species given\n"+\
+                                        "allignment species: {}".format(ID[0]))
+            lastLineA = False
+
+for openFile in writeFiles:
+    openFile.close()
+
+#print("closed all the files")
+
+print(blockNum)
+'''
+print("i: {}".format(args.iteration))
+if args.iteration+1 < len(mafFiles):
+    print("calling new iteration")
+    print("blockNum: {}".format(blockNum))
+    catchExceptions(mafFile)
+    subprocess.call("zcat "+mafFile+" | python3 testRecur.py "+str((args.iteration+1))+" "+args.mafDir+" "+str(blockNum)+" "+args.speciesDir, shell=True)
+else:
+    print("exiting out of recur: {}+1 not < {}".format(args.iteration,len(mafFiles)))
+    print("blockNum: {}".format(blockNum))
+    pass
+'''
